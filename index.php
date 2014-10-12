a0e2441042f88d7a01e5e3a20a03901e52dfc798
Gonzalo Tello <gonzalotello@iMac-de-Gonzalo.local>
2014-10-11 22:21:46 -0300

    prueba

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 /*
  *---------------------------------------------------------------
- * APPLICATION ENVIRONMENT
+ * APPLICATION ENVIRONMENTs
  *---------------------------------------------------------------
  *
  * You can load different configurations depending on your
